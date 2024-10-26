b8bc390a010ee023ca8fd4d84ec894bce86f1cf4
BoBoYanZawDev <boboyanzawdev@gmail.com>
2024-10-26 18:39:06 +0630

    fixed scroll animated js

--- js/index.js
+++ js/index.js
@@ -2,15 +2,30 @@ let section = document.querySelectorAll("section");
 let navLinks =document.querySelectorAll("header li a");
 let downloadLink = document.querySelector('header li a[href*="download"]');
 let downloadDropdownItems = document.querySelectorAll('.dropdown-menu .dropdown-item');
+
+//Scroll animation functions 
+const showSection = (entry) => {
+    entry.forEach((item) => {
+        if (item.isIntersecting) {
+            item.target.classList.add('animated'); // Add animation class
+        } else {
+            item.target.classList.remove('animated'); // Remove animation class when not visible
+        }
+    });
+};
+const observer = new IntersectionObserver(showSection, {
+    threshold: 0.2 // 20% of the section is visible
+});
+
 window.onscroll = ()=>{
         section.forEach(sec=>{
             let top = window.scrollY;
             let offset = sec.offsetTop-150;
             let height = sec.offsetHeight;
             let id=sec.getAttribute("id");
-            let Downid=document.getElementById('download');
 
-            if(top >= offset && top <+ offset +height){
+            // for section active link in header
+            if(top >= offset && top <= offset +height){
                 navLinks.forEach( links =>{
                     links.classList.remove('active');
                 });
@@ -25,10 +40,10 @@ window.onscroll = ()=>{
                         }
                     });
                 }
-                sec.classList.add('animated');
-            } else {
-                sec.classList.remove('animated');
             }
+
+            //for Section Visible animation obj
+            observer.observe(sec);
         });
     }
 document.addEventListener("DOMContentLoaded", function () {
@@ -48,6 +63,5 @@ text.forEach(char =>{
 const card = document.querySelectorAll('.section2 .cardContainer .card');
 card.forEach((comp,i)=>{
     comp.style.animationDuration=`${(i+=2)*500}ms`;
-    console.log(comp);
 }
 )
